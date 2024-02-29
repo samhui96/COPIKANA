d4c8f59b349396bb2b8b2a3f30a9ef61fa6a6030
samhui96 <samhui1996@gmail.com>
2024-02-29 00:30:05 +0000

    Added a GUI.

--- COPIKANA_project.py
+++ COPIKANA_project.py
@@ -1,16 +1,27 @@
 import pykakasi
 import pyperclip
-import keyboard
 import tkinter as tk
 
-def convert():
+def convert(): # Convert Kanji to Hiragana
     kks = pykakasi.kakasi()
-    text = pyperclip.paste()
-    result = kks.convert(text)
+    copied_text = pyperclip.paste()
+    result = kks.convert(copied_text)
+    hiragana_text = ""
     for item in result:
-        display = item['hira']
-        print(display)
-    return display
+        hiragana_text += item["hira"] + ""
+    return hiragana_text
 
-keyboard.add_hotkey('ctrl + v', convert)
-keyboard.wait('esc')
+def update_label(): # Send Hiragana to the window
+    converted_text = convert()
+    label.config(text = converted_text, font = ("", 14))
+    root.after(500, update_label)  # Update label every 0.5 second
+  
+root = tk.Tk()
+root.title("COPIKANA beta")
+root.attributes('-topmost', True)  # Set window to stay on top
+frame = tk.Frame(width = 240) # Set minimum width of the window
+label = tk.Label()
+update_label()  # Start updating label initially
+label.pack()
+frame.pack()
+root.mainloop()
