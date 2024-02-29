d130a2d222dfa0a7f8360cfb6580ceaa304388f1
samhui96 <samhui1996@gmail.com>
2024-02-29 00:51:05 +0000

    1. Added error message: 'Logo missing'. 2. Added a slider scale to change font size.

--- COPIKANA_project.py
+++ COPIKANA_project.py
@@ -1,27 +1,57 @@
 import pykakasi
 import pyperclip
+import base64
+import os
 import tkinter as tk
+from tkinter import ttk
 
-def convert(): # Convert Kanji to Hiragana
+def to_hira(): # Convert kanji to hiragana
     kks = pykakasi.kakasi()
-    copied_text = pyperclip.paste()
+    copied_text = pyperclip.paste() # Get text from clipboard
     result = kks.convert(copied_text)
-    hiragana_text = ""
-    for item in result:
-        hiragana_text += item["hira"] + ""
-    return hiragana_text
+    hira_text = ""
+    for item in result: # Keep hiragana only from result
+        hira_text += item["hira"] + "　"
+    label.config(text=hira_text)
 
-def update_label(): # Send Hiragana to the window
-    converted_text = convert()
-    label.config(text = converted_text, font = ("", 14))
-    root.after(500, update_label)  # Update label every 0.5 second
-  
+def change_size(x):
+    size = int(float(x))
+    label.config(font=("MS PGothic", size))
+
+def update_label():
+    clipboard_text = str(pyperclip.paste())
+    label_text = str(label.cget("text"))
+    if clipboard_text != label_text:
+        to_hira()
+    else:
+        pass
+    root.after(500, update_label)  
+
+# Creat a window
 root = tk.Tk()
-root.title("COPIKANA beta")
-root.attributes('-topmost', True)  # Set window to stay on top
-frame = tk.Frame(width = 240) # Set minimum width of the window
-label = tk.Label()
-update_label()  # Start updating label initially
+root.title("COPIKANA 1.0")
+root.attributes("-topmost", True)
+style = ttk.Style()
+style.theme_use('winnative')
+style.configure("Horizontal.TScale")
+
+try:
+    from COPIKANA_logo import img
+    temp = open("temp.ico", "wb+") # Create a temp file to hold icon
+    temp.write(base64.b64decode(img))
+    temp.close()
+    root.iconbitmap("temp.ico")
+    os.remove('temp.ico')
+except:
+    pyperclip.copy("Logo missing")
+    
+# Creat a silder
+slider = ttk.Scale(root, from_=17, to=51, length=240, command=change_size)
+slider.pack(anchor='nw', fill='x')
+
+# Create a label
+label = tk.Label(font=('MS PGothic', 17), wraplength=720)
+update_label()
 label.pack()
-frame.pack()
+
 root.mainloop()
